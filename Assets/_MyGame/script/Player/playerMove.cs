d2ac089e719c1c64376c157226525cd6387edbeb
a-holly1 <9870182@gmail.com>
2024-06-10 10:26:38 +0900

    2024/06/10

--- Assets/_MyGame/script/Player/playerMove.cs
+++ Assets/_MyGame/script/Player/playerMove.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 
 public class playerMove : MonoBehaviour
 {
-    public float moveSpeed = 2.0f;
+    enum eScaffold
+    {
+        none,
+
+        block,
+        ice,
+
+        scaffoldMax,
+    }
     [SerializeField]
-    Vector3 move;
-    Animator playerAnimation;
+    eScaffold scaffold = eScaffold.block;
 
     // Start is called before the first frame update
     void Start()
@@ -20,23 +27,46 @@ public class playerMove : MonoBehaviour
     void Update()
     {
         Move();
+        Direction();
     }
 
+    Vector3 move;
+    float moveSpeed;
+    float moveMax = 1.5f;
+    [SerializeField]
+    public float[] moveSpeedSetting = new float[(int)eScaffold.scaffoldMax];
+    [SerializeField]
+    float[] moveMaxSetting = new float[(int)eScaffold.scaffoldMax];
     void Move()
     {
         if (GetComponent<ObjectFall>().GetSituation() == ObjectFall.eSituation.fall) return;
 
-        transform.position += move * moveSpeed * Time.deltaTime;
+        transform.position += move * Time.deltaTime;
 
-        move = new Vector3(0.0f, 0.0f, 0.0f);
-
+        switch (scaffold)
         {
-            move.x = Input.GetAxis("Horizontal");
-            move.y = Input.GetAxis("Vertical");
+            case eScaffold.block:
+                move = new Vector3(0.0f, 0.0f, 0.0f);
+                break;
+            case eScaffold.ice:
+                //move /= 1.5f;
+                break;
         }
+        moveSpeed = moveSpeedSetting[(int)scaffold];
+        moveMax = moveMaxSetting[(int)scaffold];
+
+        move.x = Input.GetAxis("Horizontal") * moveSpeed;
+        move.y = Input.GetAxis("Vertical") * moveSpeed;
+        if (move.x > moveMax * moveSpeed) move.x = moveMax * moveSpeed;
+        if (move.y > moveMax * moveSpeed) move.y = moveMax * moveSpeed;
+
+    }
+    Animator playerAnimation;
+    void Direction()//todo:アニメーションの変更
+    {
         if (move.x * move.x > move.y * move.y)
         {
-            if (move.x < 0) playerAnimation.SetInteger("direction", 3);//todo:アニメーションの変更
+            if (move.x < 0) playerAnimation.SetInteger("direction", 3);
             else playerAnimation.SetInteger("direction", 2);
         }
         else
@@ -44,8 +74,6 @@ public class playerMove : MonoBehaviour
             if (move.y <= 0) playerAnimation.SetInteger("direction", 0);
             else playerAnimation.SetInteger("direction", 1);
         }
-
-
     }
 
     public void AddPosition(Vector3 addMove)
